3565acaeaaebf8247957f95c7ff1f4e726410325
Fernando Medina Corey <fmcorey@gmail.com>
2020-01-06 09:22:55 -0800

    Example of not showing 400s

--- handler.py
+++ handler.py
@@ -2,23 +2,11 @@ import json
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
+    print(event["body"])
+    data = json.loads(event["body"])
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "headers": {"Access-Control-Allow-Origin":"*"},
+        "body": json.dumps(data)
     }
-
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
